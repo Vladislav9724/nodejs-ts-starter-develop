d00946e7fd0a2584ae2346fac9cfb635ae658942
Vladislav9724 <vlad.tsyapka@gmail.com>
2022-08-17 20:58:20 +0300

    class5

--- src/class4.ts
+++ src/class4.ts
@@ -1,60 +1,62 @@
-interface IUser {
-    userId: number,
-    name: string,
-    city: string,
-    age: number,
-    address?: IAddress,
-
-}
-
-interface IAddress {
-    street: string,
-    house: number,
-    apartment: number
-}
-
-
-const user = {
-    userId: 1,
-    name: 'Vova',
-    city: 'Lviv',
-    age: 26,
-    address: {
-        street: 'Lvivska',
-        house: 20,
-        apartment: 91,
-    }
-}
-
-interface IGetUserKey {
-    getUserKey(): string[]
-    user: IUser
-}
-
-class User implements IGetUserKey {
-    public user: any
-
-    constructor(user: IUser) {
-        this.user = user
-    }
-
-    public getUserKey(): string[] {
-        const getUserKey: string[] = []
-        for (let key in this.user) {
-            getUserKey.push(key)
-
-            if(typeof (this.user[key]) === "object"){
-                for (key in this.user[key] ){
-                    getUserKey.push(key)
-                }
-            }
-
-        }
-        return getUserKey
-    }
-
-}
-
-const li = new User(user)
-console.log(li.getUserKey())
-console.log(li.getUserKey().length)
+// interface IUser {
+//     userId: number,
+//     name: string,
+//     city: string,
+//     age: number,
+//     address?: IAddress,
+//
+// }
+//
+// interface IAddress {
+//     street: string,
+//     house: number,
+//     apartment: number
+// }
+//
+//
+// const user = {
+//     userId: 1,
+//     name: 'Vova',
+//     city: 'Lviv',
+//     age: 26,
+//     address: {
+//         street: 'Lvivska',
+//         house: 20,
+//         apartment: 91,
+//     }
+// }
+//
+// interface IGetUserKey {
+//     getUserKey(): string[]
+//     user: IUser
+// }
+//
+// class User implements IGetUserKey {
+//     public user: IUser
+//
+//     constructor(user: IUser) {
+//         this.user = user
+//     }
+//
+//     public getUserKey(): string[] {
+//         const getUserKey: string[] = []
+//         for (let key in this.user) {
+//             getUserKey.push(key)
+//
+//
+//             if(typeof (this.user[key]) === "object"){
+//
+//                 for (let keyIf in this.user[key] ){
+//                     getUserKey.push(keyIf)
+//                 }
+//             }
+//
+//         }
+//         return getUserKey
+//     }
+//
+// }
+//
+// const li = new User(user)
+// console.log(li.getUserKey())
+// console.log(li.getUserKey().length)
