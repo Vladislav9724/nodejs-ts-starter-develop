a1f60669a701297c1a6fe5ec0cab7594919fcfbb
Vladislav9724 <vlad.tsyapka@gmail.com>
2022-10-04 12:20:43 +0300

    addTask

--- src/Class/class.ts
+++ src/Class/class.ts
@@ -1,66 +1,71 @@
-//
-//
-// const {v4: uuidv4} = require('uuid')
-//
-// interface ITask {
-//     id: string,
-//     title: string,
-//     body: string,
-//     isCompleted: boolean,
-//     createdAt: string,
-//     updatedAt: string
-// }
-//
-// const task = {
-//     id: uuidv4(),
-//     title: 'some title',
-//     body: 'some title',
-//     isCompleted: false,
-//     createdAt: new Date().toLocaleString(),
-//     updatedAt: new Date().toLocaleString()
-// }
-// const task1 = {
-//     id: uuidv4(),
-//     title: 'Hello',
-//     body: 'World',
-//     isCompleted: true,
-//     createdAt: new Date().toLocaleString(),
-//     updatedAt: new Date().toLocaleString()
-// }
-//
-// interface IToDoList {
-//
-//     adding(task: ITask): ITask|undefined
-//     // delete(id: string): ITask[]
-// }
-//
-// class ToDoList implements IToDoList {
-//
-//     tasks: ITask[] = []
-//
-//     public adding(task: ITask): ITask|undefined {
-//         this.tasks.push(task)
-//         let newTask: ITask|undefined = this.tasks.find((elem:ITask) => elem.id === task.id)
-//         return newTask
-//
-//     }
-//
-//
-//     // public delete(id: string): ITask[] {
-//     //     let filter = this.tasks.filter(value => value.id !== id)
-//     //     return filter
-//     // }
-//
-// }
-//
-// const toDoList = new ToDoList()
-//
-// const resultAdding = toDoList.adding(task)
-// const resultAdding1 = toDoList.adding(task1)
-// console.log(resultAdding)
-//
-// const id = toDoList.adding(task.id)
-// console.log(id)
-//
-// // const resultDelete = toDoList.delete(id)
-// // console.log(resultDelete)
+const {v4: uuidv4} = require('uuid')
+
+interface ITask {
+    id: string,
+    title: string,
+    body: string,
+    isCompleted: boolean,
+    createdAt: string,
+    updatedAt: string
+}
+
+const task = {
+    id: uuidv4(),
+    title: 'some title',
+    body: 'some title',
+    isCompleted: false,
+    createdAt: new Date().toLocaleString(),
+    updatedAt: new Date().toLocaleString()
+}
+const task1 = {
+    id: uuidv4(),
+    title: 'Hello',
+    body: 'World',
+    isCompleted: true,
+    createdAt: new Date().toLocaleString(),
+    updatedAt: new Date().toLocaleString()
+}
+
+interface IToDoList {
+
+    adding(task: ITask): ITask|undefined
+
+    // delete(id: ITask): string
+}
+
+class ToDoList implements IToDoList {
+
+    tasks: ITask[] = []
+
+
+    public adding(task: ITask): ITask|undefined {
+        this.tasks.push(task)
+        let newTask: ITask | undefined = this.tasks.find((elem: ITask) => elem.id === task.id)
+        try {
+            if (newTask === undefined) {
+                throw new Error()
+            }
+        }
+        catch (err){
+            console.log('There is no task')
+        }
+        return newTask
+    }
+
+
+    // public delete(id: ITask|undefined): any{
+    //
+    // }
+
+}
+
+const toDoList = new ToDoList()
+
+const resultAdding = toDoList.adding(task)
+const resultAdding1 = toDoList.adding(task1)
+console.log(resultAdding)
+
+const id= toDoList.adding(task.id)
+
+// const resultDelete = toDoList.delete(id)
+// console.log(resultDelete)
--- src/Class/uuid.ts
+++ src/Class/uuid.ts
@@ -1,67 +1,71 @@
-
-
-const {v4: uuidv4} = require('uuid')
-
-interface ITask {
-    id: string,
-    title: string,
-    body: string,
-    isCompleted: boolean,
-    createdAt: string,
-    updatedAt: string
-}
-
-const task = {
-    id: uuidv4(),
-    title: 'some title',
-    body: 'some title',
-    isCompleted: false,
-    createdAt: new Date().toLocaleString(),
-    updatedAt: new Date().toLocaleString()
-}
-const task1 = {
-    id: uuidv4(),
-    title: 'Hello',
-    body: 'World',
-    isCompleted: true,
-    createdAt: new Date().toLocaleString(),
-    updatedAt: new Date().toLocaleString()
-}
-
-interface IToDoList {
-
-    adding(task: ITask): ITask
-    // delete(id: string): string
-}
-
-class ToDoList implements IToDoList {
-
-     tasks: ITask[] = []
-
-    public adding(task: ITask): ITask {
-        this.tasks.push(task)
-        let newTask: ITask|undefined = this.tasks.find((elem:ITask) => elem.id === task.id)
-        if (newTask !== undefined){
-            return newTask
-        } else throw 'No task found'
-
-    }
-
-
-    // public delete(id: string): string {
-    //     let filter = this.tasks.filter(value => value.id !== id)
-    //     return filter
-    // }
-
-}
-
-const toDoList = new ToDoList()
-
-const resultAdding = toDoList.adding(task)
-const resultAdding1 = toDoList.adding(task1)
-console.log(resultAdding)
-
-const id = toDoList.adding(task.id)
-
-// const resultDelete = toDoList.delete(id)
-// console.log(resultDelete)
+// const {v4: uuidv4} = require('uuid')
+//
+// interface ITask {
+//     id: string,
+//     title: string,
+//     body: string,
+//     isCompleted: boolean,
+//     createdAt: string,
+//     updatedAt: string
+// }
+//
+// const task = {
+//     id: uuidv4(),
+//     title: 'some title',
+//     body: 'some title',
+//     isCompleted: false,
+//     createdAt: new Date().toLocaleString(),
+//     updatedAt: new Date().toLocaleString()
+// }
+// const task1 = {
+//     id: uuidv4(),
+//     title: 'Hello',
+//     body: 'World',
+//     isCompleted: true,
+//     createdAt: new Date().toLocaleString(),
+//     updatedAt: new Date().toLocaleString()
+// }
+//
+// interface IToDoList {
+//
+//     adding(task: ITask): ITask
+//
+//     // delete(id: ITask): string
+// }
+//
+// class ToDoList implements IToDoList {
+//
+//     tasks: ITask[] = []
+//
+//     public adding(task: ITask): ITask {
+//         this.tasks.push(task)
+//         let newTask: ITask | undefined = this.tasks.find((elem: ITask) => elem.id === task1 .id)
+//         if (newTask !== undefined)  return newTask
+//             throw 'No task found'
+//         try {
+//
+//         }
+//         catch (err){
+//
+//         }
+//     }
+//
+//
+//     // public delete(id: ITask): string {
+//
+//
+//
+//     // }
+//
+// }
+//
+// const toDoList = new ToDoList()
+//
+// const resultAdding = toDoList.adding(task)
+// const resultAdding1 = toDoList.adding(task1)
+// console.log(resultAdding)
+//
+// const id = toDoList.adding(task.id)
+//
+// // const resultDelete = toDoList.delete(id)
+// // console.log(resultDelete)
