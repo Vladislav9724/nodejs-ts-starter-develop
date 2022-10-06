64dceeba230ebab1d729ed30b8b0b683a61d058e
Vladislav9724 <vlad.tsyapka@gmail.com>
2022-10-06 14:04:58 +0300

    editingTask

--- src/Class/class.ts
+++ src/Class/class.ts
@@ -1,95 +1,86 @@
-// const {v4: uuidv4} = require('uuid')
-// const {faker} = require('@faker-js/faker');
-//
-// interface ITask {
-//     id: string,
-//     title: string,
-//     body: string,
-//     isCompleted: boolean,
-//     createdAt: string,
-//     updatedAt: string
-// }
-//
-// interface IToDoList {
-//     adding(title: string, body: string): ITask
-//
-//     find(): ITask[]
-//
-//     delete(id: string): string
-//
-//     editing(title: string, body: string):ITask[]
-// }
-//
-// class ToDoList implements IToDoList {
-//
-//     tasks: ITask[] = []
-//
-//
-//     public adding(title: string, body: string): ITask {
-//         const task = {
-//             id: uuidv4(),
-//             title: title,
-//             body: body,
-//             isCompleted: false,
-//             createdAt: new Date().toLocaleString(),
-//             updatedAt: new Date().toLocaleString()
-//         }
-//         this.tasks.push(task)
-//         const newTask: ITask | undefined = this.tasks.find((elem: ITask) => elem.id === task.id)
-//         if (newTask === undefined) {
-//             throw "NO task"
-//         }
-//         return newTask
-//
-//     }
-//
-//     public find(): ITask [] {
-//         return this.tasks
-//     }
-//
-//
-//     public delete(id: string): string {
-//         const indexId = this.tasks.findIndex(index => index.id === id)
-//         if(indexId === undefined){
-//             throw "NO TASK"
-//         }
-//         this.tasks.splice(indexId, 1)
-//         return id
-//
-//     }
-//     public editing(): ITask[] {
-//         const task = {
-//             id: uuidv4(),
-//             title: "title",
-//             body: "body",
-//             isCompleted: false,
-//             createdAt: new Date().toLocaleString(),
-//             updatedAt: new Date().toLocaleString()
-//         }
-//         const editingTask = this.tasks.map((taskEd:ITask) => ({
-//             ...taskEd,
-//             title: taskEd.id === task.id? "TITLE": taskEd.title,
-//             body: taskEd.id === task.id? "BODY": taskEd.body
-//         }));
-//
-//         return editingTask
-//
-//     }
-//
-// }
-//
-// const toDoList = new ToDoList()
-//
-// const resultAdding = toDoList.adding('title 1', 'body 1')
-// toDoList.adding(faker.word.noun(), faker.word.noun())
-// toDoList.adding(faker.word.noun(), faker.word.noun())
-// const resultEditing = toDoList.editing()
-// const resultFind = toDoList.find()
-// const resultDelete = toDoList.delete(resultAdding?.id)
-//
-//
-//
-//
-// console.log(`Result = ${JSON.stringify(resultFind, null, 2)}`)
-// console.log(`Id from the deleted task: ${resultDelete}`)
-// console.log(resultEditing)
+const {v4: uuidv4} = require('uuid')
+const {faker} = require('@faker-js/faker');
+
+interface ITask {
+    id: string,
+    title: string,
+    body: string,
+    isCompleted: boolean,
+    createdAt: string,
+    updatedAt: string
+}
+
+interface IToDoList {
+    adding(title: string, body: string): ITask
+
+    find(): ITask[]
+
+    delete(id: string): string
+
+    editing(id: string,title: string, body: string, updatedAt: string):ITask
+}
+
+class ToDoList implements IToDoList {
+
+    tasks: ITask[] = []
+
+
+    public adding(title: string, body: string): ITask {
+        const task = {
+            id: uuidv4(),
+            title: title,
+            body: body,
+            isCompleted: false,
+            createdAt: new Date().toLocaleString(),
+            updatedAt: new Date().toLocaleString()
+        }
+        this.tasks.push(task)
+        const newTask: ITask | undefined = this.tasks.find((elem: ITask) => elem.id === task.id)
+        if (newTask === undefined) {
+            throw "NO task"
+        }
+        return newTask
+
+    }
+
+    public find(): ITask [] {
+        return this.tasks
+    }
+
+
+    public delete(id: string): string {
+        const indexId = this.tasks.findIndex(index => index.id === id)
+        if(indexId === undefined){
+            throw "NO TASK"
+        }
+        this.tasks.splice(indexId, 1)
+        return id
+
+    }
+    public editing(id:string,title:string, body:string,updatedAt: string): ITask {
+        const editingTask: ITask | undefined = this.tasks.find((elem: ITask) => elem.id === id)
+        if (editingTask === undefined) {
+            throw "NO task"
+        }
+            editingTask.title =title,
+            editingTask.body = body,
+            editingTask.updatedAt = updatedAt
+
+        return editingTask
+
+    }
+
+}
+
+const toDoList = new ToDoList()
+
+const resultAdding = toDoList.adding('title 1', 'body 1')
+toDoList.adding(faker.word.noun(), faker.word.noun())
+toDoList.adding(faker.word.noun(), faker.word.noun())
+const resultFind = toDoList.find()
+const resultEditing = toDoList.editing(resultAdding?.id,'TITLE', 'BODY',new Date().toLocaleString() )
+const resultDelete = toDoList.delete(resultAdding?.id)
+
+console.log(`Result = ${JSON.stringify(resultFind, null, 2)}`)
+console.log(`Id from the deleted task: ${resultDelete}`)
+console.log(`Result editing = ${JSON.stringify(resultEditing, null, 2)}`)
